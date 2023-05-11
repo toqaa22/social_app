4653ab8d75323b16ac6c7d4263d6981c89a31a34
ibrahimmedhat <doha.kroz0@gmail.com>
2023-05-11 04:09:13 +0300

    add post and read post ibrahim medhat 10/5/2023

--- lib/views/addpost_view/add_post_cubit.dart
+++ lib/views/addpost_view/add_post_cubit.dart
@@ -1,52 +1,37 @@
 import 'dart:io';
 
-import 'package:bloc/bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:meta/meta.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:socialapp/views/models/addpost_model.dart';
-import 'package:socialapp/views/models/user_model.dart';
 
 import '../../constants.dart';
 
 part 'add_post_state.dart';
 
 class AddPostCubit extends Cubit<AddPostState> {
-  AddPostCubit({this.image,this.post,this.captionController}) : super(AddPostInitial());
+  AddPostCubit() : super(AddPostInitial());
 
   static AddPostCubit get(context) => BlocProvider.of(context);
   File? image;
   final picker = ImagePicker();
   Postmodel? post;
-  TextEditingController? captionController;
+  TextEditingController captionController = TextEditingController();
 
- void addpost(
-  {
-    required String imageUrl,required String time,required String caption
-}
-
-     ){
-   //add collection posts to docs in collection users
-   post=Postmodel(
-       imageUrl: imageUrl,
-       caption: caption,
-       time: time,
-     name: user!.name,
-     profileImage: user!.profileImage,
-
-   );
-   FirebaseFirestore.instance.collection('user').doc(user!.id).collection('posts').add(post!.toMap()).then((value){
-
-   });
-
- }
-
-
-
+  void addpost({required String imageUrl, required String time, required String caption}) {
+    //add collection posts to docs in collection users
+    post = Postmodel(
+      imageUrl: imageUrl,
+      caption: caption,
+      time: time,
+      name: user!.name,
+      profileImage: user!.profileImage,
+    );
+    FirebaseFirestore.instance.collection('user').doc(user!.id).collection('posts').add(post!.toMap()).then((value) {});
+  }
 
   Future permission({required context}) async {
     Map<Permission, PermissionStatus> statuses = await [
@@ -59,20 +44,15 @@ class AddPostCubit extends Cubit<AddPostState> {
     }
   }
 
-
-String? imageUrl;
+  String? imageUrl;
   imgFromGallery() async {
-    await picker.pickImage(
-        source: ImageSource.gallery, imageQuality: 50
-    ).then((value) {
-
-     FirebaseStorage.instance.ref().child('user/${user!.id}/${DateTime.now()}').putFile(File(value!.path)).then((value) {
-       value.ref.getDownloadURL().then((value) {
-         imageUrl=value;
-         emit(UploadPhoto());
-       });
-     });
+    await picker.pickImage(source: ImageSource.gallery, imageQuality: 50).then((value) {
+      FirebaseStorage.instance.ref().child('user/${user!.id}/${DateTime.now()}').putFile(File(value!.path)).then((value) {
+        value.ref.getDownloadURL().then((value) {
+          imageUrl = value;
+          emit(UploadPhoto());
+        });
+      });
     });
   }
 }
-
